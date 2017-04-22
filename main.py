efb4027a0a165b6aefb32449ee04ca83a85504e0
Apurva Nitanjay <apuayush@gmail.com>
2017-04-22 17:13:44 +0530

    not backend connected to frontend

--- main.py
+++ main.py
@@ -3,34 +3,32 @@ from tornado.web import RequestHandler, Application, removeslash, UIModule, asyn
 from tornado.gen import coroutine, Task, engine
 from tornado.ioloop import IOLoop
 from tornado.httpserver import HTTPServer
-from tornado.httpclient import AsyncHTTPClient
 
-#other libraries
+# other libraries
 import os
 from pymongo import MongoClient
 
-define('port',default=7777,help='run on the given port',type=int)
+define('port', default=7777, help='run on the given port', type=int)
 
-db = MongoClient("localhost",27017)['recker']
-
+db = MongoClient("localhost", 27017)['recker']
 
 
 class IndexHandler(RequestHandler):
     def get(self):
         all_categories = db.collection.find()
-        self.render('index.html', all_categories=all_categories)
+        self.render('index.html')
+        #, all_categories=all_categories)
 
 
 class ModuleHandler(UIModule):
-    def render(self, category, description, review, image_url, comment):
-        self.render_string("module.html",category=category,
-                           description=description,
-                           review=review,
-                           image_url=image_url,
-                           comment=comment
-                           )
-
-
+    def render(self, category, review, description, image_url, comment):
+        return self.render_string("module/ui.html",
+                                  category=category,
+                                  review=review,
+                                  description=description,
+                                  image_url=image_url,
+                                  comment=comment
+                                  )
 
 
 settings = dict(debug=True,
@@ -40,11 +38,10 @@ app = Application(
     handlers=[(r'/', IndexHandler)],
     template_path=os.path.join(os.path.dirname(__file__), "template"),
     static_path=os.path.join(os.path.dirname(__file__), "static"),
-    ui_modules={'single_product':ModuleHandler},
+    ui_modules={'ModuleHandler': ModuleHandler},
     **settings)
 
 if __name__ == "__main__":
     parse_command_line()
     HTTPServer(app).listen(options.port)
     IOLoop.instance().start()
-
